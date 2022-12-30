7573570455cad6ca9e57eb2aa90d838cb564c746
mansiinfi <105268882+mansiinfi@users.noreply.github.com>
2022-12-30 12:04:13 +0530

    Update LoginTest.java

--- OrangeHRM/src/test/java/UI/LoginTest.java
+++ OrangeHRM/src/test/java/UI/LoginTest.java
@@ -8,8 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-//asghj
-//zxcvbnm
+//asdfg
 public class LoginTest {
 	WebDriver driver;
 
